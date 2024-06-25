7bb1d4d3e4029678d6820c7e3fda00b7facfb8c0
Samarth Kamble <kamble.samarth91@gmail.com>
2024-06-25 22:08:21 +0530

    Implement Complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,39 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Samarth",
+    lastName: "Kamble",
+    email: "Samarthkamble2004@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1250 }, { currentBalance: 125 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
